e0042dc8b212a8167291102dd4f851580bdb738a
johannyjm <meetpastarts@gmail.com>
2019-11-20 20:04:56 +0900

    1120

--- AtCoder/ABC145_B.cpp
+++ AtCoder/ABC145_B.cpp
@@ -6,6 +6,18 @@ int main() {
     ios::sync_with_stdio(false);
     int n;
     string s;
-    cout << s[: n] << " " << s[n: ]
+    cin >> n >> s;
+    
+    if(n % 2 == 1) {
+        cout << "No" << endl;
+        return 0;
+    }
+    for (int i = 0; i < (n / 2); ++i){
+        if(s[i] != s[n / 2 + i]){
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
 
 }
