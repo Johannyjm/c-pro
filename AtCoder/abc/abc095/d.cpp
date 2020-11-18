ce0fe679c08b8f2229c7ad503ce0291deec0fbcf
Johannyjm <meetpastarts@gmail.com>
2020-11-18 23:24:29 +0900

    [add] abc095/d.cpp

--- AtCoder/abc/abc095/d.cpp
+++ AtCoder/abc/abc095/d.cpp
@@ -14,34 +14,35 @@ int main(){
     rep(i, n) cin >> x[i] >> v[i];
 
     vector<ll> sm1(n+1, 0), cmx1(n+1);
-    sm1[1] = v[0] - x[0];
-    cmx1[1] = max(0ll, sm1[1]);
-    ll prevx = x[0];
-    ll res = max(0ll, sm1[1]);
-    rep1(i, n){
-        sm1[i+1] = sm1[i] + v[i] - (x[i]-prevx);
+    ll vsm = 0;
+    ll res = 0;
+    rep(i, n){
+        vsm += v[i];
+        sm1[i+1] = vsm - x[i];
         cmx1[i+1] = max(cmx1[i], sm1[i+1]);
+
         res = max(res, sm1[i+1]);
-        prevx = x[i];
     }
 
-    for(auto e: sm1) cout << e << " ";
-    cout << endl;
+    // for(auto e: sm1) cout << e << " ";
+    // cout << endl;
+    // for(auto e: cmx1) cout << e << " ";
+    // cout << endl;
 
     vector<ll> sm2(n+1, 0), cmx2(n+1);
-    sm2[1] = v[n-1] - (c-x[n-1]);
-    cmx2[1] = max(0ll, sm2[1]);
-    res = max(res, sm2[1]);
-    prevx = x[n-1];
-    rep1(i, n){
-        sm2[i+1] = v[n-i-1] - (prevx-x[n-i-1]);
+    vsm = 0;
+    rep(i, n){
+        vsm += v[n-i-1];
+        sm2[i+1] = vsm - (c-x[n-i-1]);
         cmx2[i+1] = max(cmx2[i], sm2[i+1]);
-        res = max(res, sm2[i+1]);
-        prevx = x[n-i-1];
-    }
 
-    for(auto e: sm2) cout << e << " ";
-    cout << endl;
+        res = max(res, sm2[i+1]);
+    }
+    
+    // for(auto e: sm2) cout << e << " ";
+    // cout << endl;
+    // for(auto e: cmx2) cout << e << " ";
+    // cout << endl;
 
     rep(i, n){
         ll cand = sm1[i+1] - x[i] + cmx2[n-i-1];
@@ -49,7 +50,7 @@ int main(){
     }
 
     for(int i = n-1; i >= 0; --i){
-        ll cand = sm2[n-i] - (c-x[i]) + cmx1[i-1];
+        ll cand = sm2[n-i] - (c-x[i]) + cmx1[i];
         res = max(res, cand);
     }
 
