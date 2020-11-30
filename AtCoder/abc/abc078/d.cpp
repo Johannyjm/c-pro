2ddc2d781bdd51a82c18a264e348e6bbd12066be
Johannyjm <meetpastarts@gmail.com>
2020-11-30 22:54:46 +0900

    [add] abc78/arc085 d/b.cpp

---
+++ AtCoder/abc/abc078/d.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define rep1(i, n) for(int i = 1; i < (n); ++i)
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int n, z, w;
+    cin >> n >> z >> w;
+    vector<ll> a(n);
+    rep(i, n) cin >> a[i];
+
+    ll res = max(abs(a[n-2]-a[n-1]), abs(a[n-1]-w));
+
+    cout << res << endl;
+
+    return 0;
+}
