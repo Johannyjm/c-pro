6bc942ccfe9cbdde73024641d9379d788c167104
Yoshitaka Tomiyama <meetpastarts@gmail.com>
2021-02-07 16:21:43 +0900

    [add] abc191/e.cpp

--- AtCoder/abc/abc191/d.cpp
+++ AtCoder/abc/abc191/d.cpp
@@ -4,14 +4,87 @@
 using namespace std;
 using ll = long long;
 
+ll x, y, r;
+
+ll s2i(string s){
+    ll ret = 0;
+    int n = s.size();
+    ll offset = 10000;
+    rep(i, n){
+        if(s[i] == '.'){
+            offset = pow(10, 5-n+i);
+            continue;
+        }
+        ret *= 10;
+        ret += (s[i] - '0');
+    }
+
+    return ret * offset;
+}
+
+bool inside(ll a, ll b){
+    a *= 10000;
+    b *= 10000;
+    return (x-a)*(x-a) + (y-b)*(y-b) <= r*r;
+}
+
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
 
-    double x, y, r;
-    cin >> x >> y >> r;
+    string X, Y, R;
+    cin >> X >> Y >> R;
 
-    int l = ceil()
+    x = s2i(X);
+    y = s2i(Y);
+    r = s2i(R);
+
+    // cout << x << " " << y << " " << r << endl;
+    // cout << x*x << " " << y*y << " " << r*r << endl;
+
+    ll left = (x-r + 10000 - 1)/10000;
+    ll right = (x+r)/10000;
+    ll res = 0;
+    for(int i = left-10; i <= right+10; ++i){
+        // calc top
+        ll ok = y/10000 - 10;
+        bool nothing = false;
+        while(!inside(i, ok)){
+            ++ok;
+            if(ok == y/10000 +10){
+                nothing = true;
+                break;
+            }
+        }
+        if(nothing) continue;
+        // if(inside(i, y/10000)) ok = y/10000;
+        // else if(inside(i, (y+10000-1)/10000)) ok = (y+10000-1)/10000;
+        // else continue;
+        
+        ll ok2 = ok;
+        ll ng = (y+r)/10000 + 10;
+        while(ng - ok > 1){
+            ll mid = (ok + ng)/2;
+
+            if(inside(i, mid)) ok = mid;
+            else ng = mid;
+        }
+        ll top = ok;
+
+        // calc bottom
+        ll ng2 = (y-r)/10000 - 10;
+        while(ok2 - ng2 > 1){
+            ll mid = (ok2 + ng2)/2;
+
+            if(inside(i, mid)) ok2 = mid;
+            else ng2 = mid;
+        }
+        ll bottom = ok2;
+        // cout << i << " " << top << " " << bottom << endl;
+        res += top - bottom + 1;
+    }
+
+    cout << res << endl;
 
     return 0;
 }
--- AtCoder/abc/abc191/old/d2.cpp
+++ AtCoder/abc/abc191/old/d2.cpp
@@ -2,6 +2,23 @@
 #include <math.h>
 #include <iostream>
 using namespace std;
+using ll = long long;
+
+ll s2i(string s){
+    ll ret = 0;
+    int n = s.size();
+    ll offset = 10000;
+    for(int i = 0; i < n; ++i){
+        if(s[i] == '.'){
+            offset = pow(10, 5-n+i);
+            continue;
+        }
+        ret *= 10;
+        ret += (s[i] - '0');
+    }
+
+    return ret * offset;
+}
 
 int mceil(double d){
     int ret = ceil(d);
@@ -20,31 +37,38 @@ int mfloor(double d){
 
 int main(void){
 
-double a,b,r,p;
-int start,end,top,bottom,i,j;
+// double a,b,r,p;
+ll start,end,top,bottom,i,j;
 long long num;
 
 num=0;
 
 // printf("a b r =");
 // scanf("%lf %lf %lf",&a,&b,&r);
-cin >> a >> b >> r;
+// cin >> a >> b >> r;
+string X, Y, R;
+cin >> X >> Y >> R;
 
-start = mceil(a-r);
-end = mfloor(a+r);
+ll x = s2i(X);
+ll y = s2i(Y);
+ll r = s2i(R);
+
+start = ((x-r)+10000-1)/10000;
+end = (x+r)/10000;
 
 for(i=start;i<=end;i++){
 
-p = sqrt(pow(r,2)-pow((a-(double)i),2));
+double p = sqrt(pow(r,2)-pow((x-i*10000),2));
 
 // cf(b,p,&bottom,&top);
-bottom = mceil(b-p);
-top = mfloor(b+p);
+bottom = mceil((y-p)/10000);
+top = mfloor((y+p)/10000);
 
 for(j=bottom;j<=top;j++){
 num = num + 1;
 }
 
+// cout << i << " " << top << " " << bottom << endl;
 }
 // printf("中心:(%lf,%lf) 半径:%lf 格子点の数: %d \n",a,b,r,num);
 cout << num << endl;
