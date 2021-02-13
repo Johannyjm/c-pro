d9e6c78152f82a458f1623aadb9111afaf7110a3
Johannyjm <meetpastarts@gmail.com>
2021-02-13 16:20:12 +0900

    [add] abc042/d.cpp

---
+++ AtCoder/abc/abc042/d.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define rep1(i, n) for(int i = 1; i < (n); ++i)
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = modint1000000007;
+
+vector<mint> fact, ifact, double_fact;
+void init(int n) {
+    fact.resize(n+1);
+    ifact.resize(n+1);
+    double_fact.resize(n+1);
+    fact[0] = 1;
+
+    for(int i = 1; i <= n; ++i) fact[i] = fact[i-1] * i;
+
+    ifact[n] = fact[n].inv();
+    for(int i = n; i >= 1; --i) ifact[i-1] = ifact[i] * i;
+
+    double_fact[1] = 1;
+    for(int i = 3; i <= n; i += 2) double_fact[i] = double_fact[i-2] * i;
+}
+
+mint comb(int n, int k){
+    return fact[n] * ifact[k] * ifact[n-k];
+}
+mint perm(int n, int k){
+    return fact[n] * ifact[n-k];
+}
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    init(220000);
+
+    int h, w, a, b;
+    cin >> h >> w >> a >> b;
+
+    mint edge = comb(h-a+b-1, b);
+    mint res = edge * comb(a+w-b-1, a);
+    for(int j = b+1; j < w; ++j){
+        mint base = comb(h-a-1+j, j) - edge;
+        // cout << j << " " << a+w-j-2 << " " << base.val() << " " << comb(a+w-j-2, a-1).val() << endl;
+        res += base * comb(a-1+w-j-1, a-1);
+    }
+
+    cout << res.val() << endl;
+
+    return 0;
+}
