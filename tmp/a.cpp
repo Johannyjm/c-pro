53a9050818a45bf37ea0e1ad323e9918d633e42e
Johannyjm <meetpastarts@gmail.com>
2021-03-22 23:16:24 +0900

    [add] bfs

--- tmp/a.cpp
+++ tmp/a.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 #include <queue>
 using namespace std;
+
+const int dy[] = {0, 1, 0, -1};
+const int dx[] = {1, 0, -1, 0};
 
 int main(){
     cin.tie(nullptr);
@@ -10,25 +13,37 @@ int main(){
 
     int h, w;
     cin >> h >> w;
-    vector<string> grid(h);
-    for(int i = 0; i < h; ++i) cin >> grid[i];
-
+    vector<string> s(h);
+    for(int i = 0; i < h; ++i) cin >> s[i];
+    
     const int INF = 1001001001;
     vector<vector<int>> dist(h, vector<int>(w, INF));
     dist[0][0] = 0;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push({0, 0});
+
+    queue<pair<int, int>> q;
+    q.push({0, 0});
 
     while(!q.empty()){
-        pair<int, int> p = pq.top();
-        pq.pop();
+        pair<int, int> p = q.front();
+        q.pop();
 
-        int v = p.second;
+        int y = p.first;
+        int x = p.second;
+        for(int dir = 0; dir < 4; ++dir){
+            int ny = y + dy[dir];
+            int nx = x + dx[dir];
 
-        
+            if(ny<0 || ny>=h || nx<0 || nx>=w) continue;
+            if(s[ny][nx] == '#') continue;
+            if(dist[ny][nx] != INF) continue;
+
+            dist[ny][nx] = dist[y][x] + 1;
+            q.push({ny, nx});
+        }
     }
 
-    cout << 0 << endl;
+    cout << dist[h-1][w-1] << endl;
+
 
     return 0;
 }
