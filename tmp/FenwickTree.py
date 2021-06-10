98dab9ed3881f6c0a890a73332d3acd22e6306db
Johannyjm <meetpastarts@gmail.com>
2021-06-10 16:06:24 +0900

    [add] tmp/FenwickTree.py

---
+++ tmp/FenwickTree.py
@@ -0,0 +1,40 @@
+class Fenwick_Tree:
+
+    def __init__(self, n):
+        self._n = n
+        self.bit = [0] * n
+    
+    def add(self, idx, x):
+        idx += 1
+
+        while idx <= self._n:
+            self.bit[idx - 1] += x
+            idx += idx & -idx
+        
+    def sum(self, l, r):
+        return self._sum(r) - self._sum(l)
+    
+    def _sum(self, idx):
+        ret = 0
+        
+        while idx > 0:
+            ret += self.bit[idx - 1]
+            idx -= idx & -idx
+        
+        return ret
+    
+
+n, q = map(int, input().split())
+a = list(map(int, input().split()))
+
+bit = Fenwick_Tree(n)
+for i in range(n):
+    bit.add(i, a[i])
+
+for _ in range(q):
+    cmd, x, y = map(int, input().split())
+
+    if cmd == 0:
+        bit.add(x, y)
+    if cmd == 1:
+        print(bit.sum(x, y))
