a4c1e9a71d9ef5cf15f36c4d1bd1b3353ed0e6bd
Johannyjm <meetpastarts@gmail.com>
2021-06-22 12:15:23 +0900

    [add] dij.cpp

---
+++ tmp/dij.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+struct Edge{
+    int to;
+    ll weight;
+    Edge(int t, ll w): to(t), weight(w) {}
+};
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<Edge>> g(n);
+    for(int i = 0; i < m; ++i){
+        int a, b;
+        ll c;
+        cin >> a >> b >> c;
+        
+        --a;
+        --b;
+
+        g[a].push_back(Edge(b, c));
+        g[b].push_back(Edge(a, c));
+    }
+
+    const ll INF = 1ll << 60;
+    vector<ll> dist(n, INF);
+
+    priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq;
+    pq.push({0, 0});
+
+    while(!pq.empty()){
+        auto [d, v] = pq.top();
+        pq.pop();
+
+        if(d != dist[v]) continue;
+
+        for(auto ne: g[v]){
+            int nv = ne.to;
+            if(dist[nv] > dist[v] + ne.weight){
+                dist[nv] = dist[v] + ne.weight;
+                pq.push({dist[nv], nv});
+            }
+        }
+
+    }
+
+    cout << dist[n-1] << endl;
+
+
+    return 0;
+}
