1b291ba243c074fd260690009c269ac76bf17f73
Johannyjm <meetpastarts@gmail.com>
2021-07-08 10:05:31 +0900

    [add] py

--- tmp/dij.cpp
+++ tmp/dij.cpp
@@ -16,22 +16,20 @@ int main(){
 
     int n, m;
     cin >> n >> m;
+
     vector<vector<Edge>> g(n);
     for(int i = 0; i < m; ++i){
         int a, b;
         ll c;
         cin >> a >> b >> c;
 
-        --a;
-        --b;
         g[a].push_back(Edge(b, c));
         g[b].push_back(Edge(a, c));
     }
 
     priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq;
     pq.push({0, 0});
-
-    const ll INF = 1ll << 60;
+    const ll INF = 1ll<<60;
     vector<ll> dist(n, INF);
     dist[0] = 0;
 
@@ -40,17 +38,19 @@ int main(){
         pq.pop();
 
         if(dist[v] != d) continue;
+
         for(auto ne: g[v]){
-            int nv= ne.to;
+            int nv = ne.to;
             if(dist[nv] > dist[v] + ne.weight){
                 dist[nv] = dist[v] + ne.weight;
                 pq.push({dist[nv], nv});
             }
         }
     }
-
+    
     ll res = dist[n-1];
     if(res == INF) res = -1;
+
     cout << res << endl;
 
     return 0;
---
+++ tmp/dij.py
@@ -0,0 +1,3 @@
+from heapq import heappush, heappop
+n, m = map(int, input().split())
+
