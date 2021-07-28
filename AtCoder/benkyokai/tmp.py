180c8921315dac48778718e227212f8fa9d1f349
Johannyjm <meetpastarts@gmail.com>
2021-07-28 16:50:55 +0900

    [mod] tmp.py

--- AtCoder/benkyokai/tmp.py
+++ AtCoder/benkyokai/tmp.py
@@ -1,21 +1,27 @@
-def digit_len(num: int) -> int:
-    ret = 0
-    while num:
-        ret += 1
-        num //= 10
-    
-    return ret
-
 def main():
-    n = int(input())
+    n, x = map(int, input().split())
+    a = list(map(int, input().split()))
 
-    res = 0
-    for i in range(1, n+1):
-        if digit_len(i) % 2 == 1:
-            res += 1
-    
+    for i in range(1<<n):
+        use = [0] * n
 
-    print(res)
+        for j in range(n):
+
+            if i & (1<<j):
+                use[j] = 1
+        
+        # print(use)
+
+        sm = 0
+        for j in range(n):
+            if use[j] == 0: continue
+            sm += a[j]
+        
+        if sm == x:
+            print("Yes")
+            print(use)
+            exit()
+    print("No")        
 
 if __name__ == '__main__':
     main()
