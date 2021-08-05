2c7cfbe776528445aa5709c511d8401daedf1a39
Johannyjm <meetpastarts@gmail.com>
2021-08-05 17:34:09 +0900

    [add] 023d.cpp

--- AtCoder/benkyokai/023d.cpp
+++ AtCoder/benkyokai/023d.cpp
@@ -1,26 +1,39 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
 using ll = long long;
 
 int n;
 vector<ll> h, s;
 bool check(ll mid){
+    vector<ll> limit(n);
+    for(int i = 0; i < n; ++i){
+        if(h[i] > mid) return false;
+        else limit[i] = (mid - h[i]) / s[i];
+    }
     
+    sort(limit.begin(), limit.end());
+
+    for(int i = 0; i < n; ++i){
+        if(limit[i] < i) return false;
+    }
+    return true;
 }
 
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
 
+    cin >> n;
+    
     h.resize(n);
     s.resize(n);
     for(int i = 0; i < n; ++i) cin >> h[i] >> s[i];
 
     ll ok = 1ll << 60;
     ll ng = 0;
-    
+
     while(ok - ng > 1){
         ll mid = (ok + ng) / 2;
 
@@ -33,6 +46,7 @@ int main(){
     }
 
     cout << ok << endl;
+
 
     return 0;
 }
