71686883078c78081e7bfce99fcb9be7ca1e8b1f
Johannyjm <meetpastarts@gmail.com>
2021-08-10 10:23:51 +0900

    [add] abc208/d.cpp

--- AtCoder/abc/abc208/d.cpp
+++ AtCoder/abc/abc208/d.cpp
@@ -1,6 +1,5 @@
-#include <bits/stdc++.h>
-#define rep(i, n) for(int i = 0; i < (n); ++i)
-#define rep1(i, n) for(int i = 1; i < (n); ++i)
+#include <iostream>
+#include <vector>
 using namespace std;
 using ll = long long;
 
@@ -8,26 +7,33 @@ int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
 
-    int n, m;
+    ll n, m;
     cin >> n >> m;
 
-    vector<ll> a(m), b(m), c(m);
-    rep(i, m) cin >> a[i] >> b[i] >> c[i];
-
-    const ll INF = 1ll << 60;
+    const ll INF = 1001001001;
     vector<vector<ll>> dist(n, vector<ll>(n, INF));
-    rep(i, n) dist[i][i] = 0;
-    rep(i, m){
-        dist[a[i]-1][b[i]-1] = c[i];
-    }
     
-    ll res = 0;
-    rep(k, n) for(int i = n-1; i >= 0; --i) for(int j = n-1; j >= 0; --j){
-        if(dist[i][j] != INF) res += dist[i][j];
-        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+    for(int i = 0; i < n; ++i) dist[i][i] = 0;
+    for(int i = 0; i < m; ++i){
+        ll a, b, c;
+        cin >> a >> b >> c;
+        --a;
+        --b;
+        dist[a][b] = c;
     }
-    cout << res << endl;
 
+    ll res = 0;
+    for(int k = 0; k < n; ++k){
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < n; ++j){
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                
+                if(dist[i][j] != INF) res += dist[i][j];
+            }
+        }
+    }
+
+    cout << res << endl;
 
     return 0;
 }
