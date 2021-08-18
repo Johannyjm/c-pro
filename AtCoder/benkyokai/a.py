ac176f8c3af3a3a7f032ae20d35fd270b1b7a279
Johannyjm <meetpastarts@gmail.com>
2021-08-18 22:51:36 +0900

    [add] benkyokai

--- AtCoder/benkyokai/a.py
+++ AtCoder/benkyokai/a.py
@@ -1,5 +1,31 @@
 def main():
+
+    def check(x):
+        cnt = 0
+
+        for i in range(n):
+            if a[i] * f[i] <= x: continue
+
+            cnt += a[i] - (x//f[i])
+
+        return cnt <= k
     
+    n = int(input())
+    hs = [list(map(int, input().split())) for _ in range(n)]
+
+    
+    ok = 1 << 60
+    ng = -1
+
+    while ok - ng > 1:
+        mid = (ok + ng) // 2
+
+        if check(mid):
+            ok = mid
+        else:
+            ng = mid
+
+    print(ok)
 
 
 
