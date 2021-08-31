9be07300de2ddbb502d7cef507da963aac7f4a10
Johannyjm <meetpastarts@gmail.com>
2021-08-31 16:25:57 +0900

    [add] benkyokai

--- AtCoder/benkyokai/a.py
+++ AtCoder/benkyokai/a.py
@@ -1,17 +1,33 @@
+import sys
+sys.setrecursionlimit(10**6)
+
 def main():
-    n = int(input())
 
-    res = 0
-    for i in range(1, 10 ** 7):
-        num = str(i) * 2
-        if int(num) <= n:
-            res += 1
-        else:
-            break
+    def dfs(v):
+        seen[v] = True
+        for nv in g[v]:
+            if seen[nv]: continue
+
+            dfs(nv)
+
+
+    n, m = map(int, input().split())
+
+    g = [[] for _ in range(n)]
+    for _ in range(m):
+        a, b = map(lambda x: int(x)-1, input().split())
+        g[a].append(b)
+        g[b].append(a)
     
-    print(res)
+    res = 0
+    seen = [False] * n
+    for i in range(n):
+        if seen[i]: continue
 
-
+        dfs(i)
+        res += 1
+    
+    print(res-1)
 
 if __name__ == '__main__':
     main()
