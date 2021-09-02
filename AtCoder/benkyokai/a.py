27e242f4973bbc917ce38f540d7a7442eb897a04
Johannyjm <meetpastarts@gmail.com>
2021-09-02 13:40:30 +0900

    [add] a.py

--- AtCoder/benkyokai/a.py
+++ AtCoder/benkyokai/a.py
@@ -4,30 +4,14 @@ sys.setrecursionlimit(10**6)
 def main():
 
     def dfs(v):
-        seen[v] = True
-        for nv in g[v]:
-            if seen[nv]: continue
+        for c in ("a", "b", "c"):
+            if len(v) != n-1:
+                dfs(v + c)
+            else:
+                print(v + c)
 
-            dfs(nv)
-
-
-    n, m = map(int, input().split())
-
-    g = [[] for _ in range(n)]
-    for _ in range(m):
-        a, b = map(lambda x: int(x)-1, input().split())
-        g[a].append(b)
-        g[b].append(a)
-    
-    res = 0
-    seen = [False] * n
-    for i in range(n):
-        if seen[i]: continue
-
-        dfs(i)
-        res += 1
-    
-    print(res-1)
+    n = int(input())
+    dfs("")
 
 if __name__ == '__main__':
     main()
