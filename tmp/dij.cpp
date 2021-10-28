5e3c2306d8495255915d4431b9529fb382ad5496
Johannyjm <meetpastarts@gmail.com>
2021-10-28 16:11:59 +0900

    [add] dij...

--- tmp/dij.cpp
+++ tmp/dij.cpp
@@ -11,13 +11,13 @@ struct Edge{
 };
 
 int main(){
-    cin::tie(nullptr);
+    cin.tie(nullptr);
     ios::sync_with_stdio(false);
 
     int n, m;
     cin >> n >> m;
-
     vector<vector<Edge>> g(n);
+
     for(int i = 0; i < m; ++i){
         int a, b;
         ll c;
@@ -29,9 +29,6 @@ int main(){
 
     priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq;
     pq.push({0, 0});
-    const ll INF = 1ll << 60;
-    vector<ll> dist(n, INF);
-    dist[0] = 0;
 
     while(!pq.empty()){
         auto [d, v] = pq.top();
@@ -40,11 +37,10 @@ int main(){
         if(dist[v] != d) continue;
 
         for(auto ne: g[v]){
-
             int nv = ne.to;
+
             if(dist[nv] > dist[v] + ne.weight){
                 dist[nv] = dist[v] + ne.weight;
-
                 pq.push({dist[nv], nv});
             }
         }
@@ -52,7 +48,7 @@ int main(){
 
     ll res = dist[n-1];
     if(res == INF) res = -1;
-    
+
     cout << res << endl;
 
     return 0;
