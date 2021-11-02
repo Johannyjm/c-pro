0de7375d33fa31610979411fccfe46f1cfba298b
Johannyjm <meetpastarts@gmail.com>
2021-11-02 15:56:34 +0900

    [add] lunakey mini

--- tmp/dij.cpp
+++ tmp/dij.cpp
@@ -16,19 +16,24 @@ int main(){
 
     int n, m;
     cin >> n >> m;
-    vector<vector<Edge>> g(n);
 
+    vector<vector<Edge>> g(n);
     for(int i = 0; i < m; ++i){
         int a, b;
         ll c;
         cin >> a >> b >> c;
-
-        g[a].push_back(Edge(b, c));
-        g[b].push_back(Edge(a, c));
+        --a;
+        --b;
+        g[a].push_back({b, c});
+        g[b].push_back({a, c});
     }
 
     priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq;
     pq.push({0, 0});
+
+    const ll INF = 1ll << 60;
+    vector<ll> dist(n, INF);
+    dist[0] = 0;
 
     while(!pq.empty()){
         auto [d, v] = pq.top();
@@ -38,15 +43,16 @@ int main(){
 
         for(auto ne: g[v]){
             int nv = ne.to;
-
             if(dist[nv] > dist[v] + ne.weight){
                 dist[nv] = dist[v] + ne.weight;
+
                 pq.push({dist[nv], nv});
             }
         }
     }
 
     ll res = dist[n-1];
+
     if(res == INF) res = -1;
 
     cout << res << endl;
