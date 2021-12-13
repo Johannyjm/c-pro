25733f5ee0486b5a18aa32a67fcecdb7ead92125
Johannyjm <meetpastarts@gmail.com>
2021-12-13 10:20:25 +0900

    [add] bench=benkyokai

--- tmp/a.py
+++ tmp/a.py
@@ -1,36 +1,44 @@
-# a = [
-#     [1, 2, 3], 
-#     [2, 3, 4], 
-#     [3, 4, 5]
-# ]
-# b = [
-#     [6, 5, 3], 
-#     [2, 6, 4], 
-#     [3, 7, 8]
-# ]
+n, m, x = map(int, input().split())
 
-n, m, l = map(int, input().split())
-a = [list(map(int, input().split())) for _ in range(n)]
-b = [list(map(int, input().split())) for _ in range(l)]
+c = [0] * n
+a = [[0] * m for _ in range(n)] # a = [[0] * m] * n
 
+for i in range(n):
+    c[i], *a[i] = map(int, input().split())
 
+res = 1001001001 # float('int')
+for i in range(1 << n):
+    
+    use = [False] * n
+    for j in range(n):
+        
+        if i & (1<<j):
+            use[j] = True
 
-def matmul(x, y):
-    n = len(x)
-    l = len(y)
-    m = len(y[0])
-    assert len(x[0]) == len(y), "Invalid shape!!!"
+    ability = [0] * m
+    for j in range(n):
+        
+        if use[j]:
+            for k in range(m):
+                ability[k] += a[j][k]
 
-    ret = [[0] * m for _ in range(n)]
+    valid = True
+    for k in range(m):
+        if ability[k] < x:
+            valid = False
 
-    for i in range(n):
-        for j in range(m):
-            for k in range(l):
-                ret[i][j] += a[i][k] * b[k][j]
+    if not valid:
+        continue
 
-    return ret
+    cost = 0
+    for j in range(n):
+        
+        if use[j]:
+            cost += c[j]
 
-answer = matmul(a, b)
+    res = min(res, cost)
 
-for elem in answer:
-    print(elem)
+if res == 1001001001:
+    res = -1
+
+print(res)
