46485b484537993f7e02860e5b9017bb93e98928
Johannyjm <meetpastarts@gmail.com>
2022-01-03 22:11:08 +0900

    [add] bench0103_1

--- tmp/dij.cpp
+++ tmp/dij.cpp
@@ -17,14 +17,13 @@ int main(){
 
     int n, m;
     cin >> n >> m;
-    
+
     vector<vector<Edge>> g(n);
     for(int i = 0; i < m; ++i){
         int a, b;
         ll c;
+        cin >> a >> b >> c;
 
-        --a;
-        --b;
         g[a].push_back(Edge(b, c));
         g[b].push_back(Edge(a, c));
     }
@@ -45,15 +44,15 @@ int main(){
         for(auto ne: g[v]){
             int nv = ne.to;
 
-            if(dist[nv] > dist[v] + ne.weight){
+            if(dist[nv] < dist[v] + ne.weight){
                 dist[nv] = dist[v] + ne.weight;
 
                 pq.push({dist[nv], nv});
             }
         }
     }
-    
-    ll res = dist[n-1];
+
+    ll res = dist.back();
 
     if(res == INF) res = -1;
 
