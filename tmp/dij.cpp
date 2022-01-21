175becbe43c3343eeb37c3aad73b77ae1ac3d679
Johannyjm <meetpastarts@gmail.com>
2022-01-21 13:21:12 +0900

    [add] bench0121_3

--- tmp/dij.cpp
+++ tmp/dij.cpp
@@ -17,7 +17,6 @@ int main(){
 
     int n, m;
     cin >> n >> m;
-
     vector<vector<Edge>> g(n);
     for(int i = 0; i < m; ++i){
         int a, b;
@@ -62,4 +61,3 @@ int main(){
 
     return 0;
 }
-
