8c91783971ac3a3c638bca38e040dce33a8bd622
Johannyjm <meetpastarts@gmail.com>
2022-01-24 11:27:00 +0900

    [add] bench0124_2

--- tmp/dij.cpp
+++ tmp/dij.cpp
@@ -19,7 +19,6 @@ int main(){
     cin >> n >> m;
 
     vector<vector<Edge>> g(n);
-
     for(int i = 0; i < m; ++i){
         int a, b;
         ll c;
@@ -55,9 +54,7 @@ int main(){
         }
     }
 
-    ll res = dist.back();
-
-    if(res == INF) res = -1;
+    ll res = dist[n-1];
 
     cout << res << endl;
 
