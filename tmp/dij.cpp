68732194b087eb82b1d35c54bc75883cb8324a69
Johannyjm <meetpastarts@gmail.com>
2022-02-21 15:29:45 +0900

    [add] bench0221

--- tmp/dij.cpp
+++ tmp/dij.cpp
@@ -42,7 +42,7 @@ int main(){
         pq.pop();
 
         if(dist[v] != d) continue;
-        
+
         for(auto ne: g[v]){
             int nv = ne.to;
 
@@ -54,7 +54,7 @@ int main(){
         }
     }
 
-    ll res = dist[n-1];
+    ll res = dist.back();
 
     if(res == INF) res = -1;
 
