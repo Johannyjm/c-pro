fa588d27bcdd3731cf7748183b2fc50381d6eb61
Johannyjm <meetpastarts@gmail.com>
2022-03-04 13:49:30 +0900

    [add] abc236_d

--- AtCoder/abc/abc236/d.cpp
+++ AtCoder/abc/abc236/d.cpp
@@ -5,11 +5,37 @@ using namespace std;
 using ll = long long;
 
 int n;
-vector<vector<ll>> a, dp;
-ll rec(ll s, ll v){
-    if(dp[s][v] != -1) return dp[s][v];
-    
+vector<vector<ll>> a;
+vector<bool> seen;
+ll res;
+void dfs(ll now, vector<bool> seen){
 
+    int cnt = 0;
+    rep(i, 2*n){
+        if(seen[i]) ++cnt;
+    }
+
+    if(cnt == 2*n){
+        res = max(res, now);
+        return;
+    }
+
+    int one = -1;
+    rep(i, 2*n){
+        if(seen[i]) continue;
+        one = i;
+        break;
+    }
+
+    seen[one] = true;
+    rep(two, 2*n){
+        if(seen[two]) continue;
+        if(two <= one) continue;
+
+        vector<bool> tmp = seen;
+        tmp[two] = true;
+        dfs(now ^ a[one][two], tmp);
+    }
 }
 
 int main(){
@@ -22,13 +48,11 @@ int main(){
         for(int j = i+1; j < 2*n; ++j) cin >> a[i][j];
     }
 
-    // rep(i, 2*n){
-        // rep(j, 2*n) cout << a[i][j] << " ";
-        // cout << endl;
-    // }
+    res = 0;
+    seen.resize(2*n, false);
+    dfs(0, seen);
 
-    dp.resize((1<<(2*n)), vector<ll>(2*n, -1));
-    cout << rec(0, 0) << endl;
+    cout << res << endl;
 
     return 0;
 }
