085eba7dab90d8d4cf756645fa408c5ea4444644
Johannyjm <meetpastarts@gmail.com>
2022-03-12 20:57:17 +0900

    [add] abc242

---
+++ AtCoder/abc/abc242/b.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define rep1(i, n) for(int i = 1; i < (n); ++i)
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    string s;
+    cin >> s;
+
+    sort(s.begin(), s.end());
+
+    cout << s << endl;
+
+    return 0;
+}
