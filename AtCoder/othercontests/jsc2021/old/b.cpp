d242ea03cfc88b138a98b6e781a40e80222a29f7
Johannyjm <meetpastarts@gmail.com>
2022-03-23 09:50:22 +0900

    [add] jsc2021

--- AtCoder/othercontests/jsc2021/old/b.cpp
+++ AtCoder/othercontests/jsc2021/old/b.cpp
@@ -4,25 +4,11 @@
 using namespace std;
 using ll = long long;
 
-
-
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
 
-    int a, b;
-    cin >> a >> b;
-
-    int res = 1;
-    for(int g = 2; g <= b; ++g){
-
-        int l = 0;
-        while(l < a) l += g;
-        int r = l + g;
-        if(a<=l && r<=b) res = max(res, g);
-    }
-
-    cout << res << endl;
+    
 
     return 0;
 }
