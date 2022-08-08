412d6c5780879dba7c28bfb8a5d6f838b2c74120
Johannyjm <meetpastarts@gmail.com>
2022-08-09 00:38:38 +0900

    [add] birthday problem

--- tmp/a.py
+++ tmp/a.py
@@ -1,50 +1,18 @@
-import sys
-sys.setrecursionlimit(10**6)
-
-class DisjointSetUnion:
-
-    def __init__(selg, n: int):
-        self.n = n
-        self.p = [-1 for _ in range(n)]
-        self.r = [1 for _ in range(n)]
-
-    def leader(self, x: int):
-        if self.p[x] == -1:
-            return x
-        else:
-            self.p[x] = self.leader(self.p[x])
-            return self.p[x]
-
-    def merge(self, x: int, y: int):
-        x = self.leader(x)
-        y = self.leader(y)
-
-        if x == y:
-            return
-
-        if self.r[x] > self.r[y]:
-            x, y = y, x
-
-        if self.r[x] == self.r[y]:
-            self.r[y] += 1
-
-        self.p[x] = y
-
 def main():
-    n, q = map(int, input().split())
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
 
-    uf = DisjointSetUnion(n)
-    for _ in range(q):
-        p, a, b = map(int, input().split())
+    count = dict()
+    res = -1
+    for elem in a:
 
-        if p == 0:
-            uf.merge(a, b)
-        
-        if p == 1:
-            if uf.leader(a) == uf.leader(b):
-                print("Yes")
-            else:
-                print("No")
+        if elem - k in count:
+            count[elem] = count[elem - k] + 1
+            res = max(res, count[elem])
+        else:
+            count[elem] = 1
+    
+    print(res)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
