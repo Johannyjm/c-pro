a737cb9cda55362ddfd833a1be51c6c39ec51a81
Johannyjm <meetpastarts@gmail.com>
2022-08-17 00:25:07 +0900

    [add] bench0817_3'18

--- tmp/a.py
+++ tmp/a.py
@@ -1,18 +1,30 @@
+from collections import defaultdict
+
 def main():
+    # n: 13, k: 3
+    # [-2 3 4 1 2 2 4 8 3 7 4 10 1]
+    # → 5
+    # dp[element] := elementが等差数列の最後尾になるときの長さ
+    # dp[element] = dp[element - k] + 1
+
+
+
+
     n, k = map(int, input().split())
     a = list(map(int, input().split()))
 
-    count = dict()
-    res = -1
+    dp = defaultdict(int)
     for elem in a:
 
-        if elem - k in count:
-            count[elem] = count[elem - k] + 1
-            res = max(res, count[elem])
-        else:
-            count[elem] = 1
+        dp[elem] = dp[elem - k] + 1
     
-    print(res)
+    res = max(dp.values())
+
+    if res < 2:
+        print(-1)
+    else:
+        print(res)    
+
 
 if __name__ == '__main__':
     main()
