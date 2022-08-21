42afe2076e3af981fc1b6939b9f5597bfdffc7ba
Johannyjm <meetpastarts@gmail.com>
2022-08-22 00:14:01 +0900

    [add] abc265

--- AtCoder/abc/abc265/d.py
+++ AtCoder/abc/abc265/d.py
@@ -1,5 +1,23 @@
 from itertools import accumulate
 
+def binary_search(key, a):
+    n = len(a)
+
+    l = -1
+    r = n
+    while r-l > 1:
+        m = (l+r) // 2
+
+        if a[m] == key:
+            return m
+        
+        if a[m] < key:
+            l = m
+        else:
+            r = m
+    
+    return -1
+
 def main():
     n, p, q, r = map(int, input().split())
     a = list(map(int, input().split()))
@@ -9,21 +27,37 @@ def main():
 
     x, y, z, w = -1, -1, -1, -1
 
-    l = 0
-    while l < n:
-
-        r = l+1
-
-        while s[r] - s[l] < p+q+r:
-            r += 1
+    for l in range(n):
         
-        if s[r] - s[l] == p+q+r:
-            x = r
-            w = l
-            break
+        key = p+q+r - s[l]
+        
+        idx = binary_search(key, s)
 
-        else:
-            l += 1
+        if idx != -1:
+            print(l, r, s[l], s[r], key)
+            x, w = l, r
+            break
+    
+    print(x, y, z, w)
+
+
+
+
+    # l = 0
+    # while l < n:
+
+    #     r = l+1
+
+    #     while s[r] - s[l] < p+q+r:
+    #         r += 1
+        
+    #     if s[r] - s[l] == p+q+r:
+    #         x = r
+    #         w = l
+    #         break
+
+    #     else:
+    #         l += 1
 
 if __name__ == '__main__':
     main()
