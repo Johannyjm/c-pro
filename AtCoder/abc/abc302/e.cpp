7ef4c212088aea3fa238ade7d50c1ca54483fbe4
Johannyjm <meetpastarts@gmail.com>
2023-06-04 20:52:45 +0900

    [add] abc302/e.cpp

--- AtCoder/abc/abc302/e.cpp
+++ AtCoder/abc/abc302/e.cpp
@@ -1,6 +1,12 @@
-#include<iostream>
-#include<set>
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define rep1(i, n) for(int i = 1; i < (n); ++i)
 using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = modint1000000007;
+
 
 const int MAX_N = 3e5 + 5;
 int edgeCount[MAX_N];
